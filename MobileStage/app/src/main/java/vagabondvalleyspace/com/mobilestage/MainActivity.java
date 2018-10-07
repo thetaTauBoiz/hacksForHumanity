b67084adacd29af22a24b15d5a22d0fee714536d
bull313 <=>
2018-10-07 00:40:08 -0700

    Added alert functionality

--- MobileStage/app/src/main/java/vagabondvalleyspace/com/mobilestage/MainActivity.java
+++ MobileStage/app/src/main/java/vagabondvalleyspace/com/mobilestage/MainActivity.java
@@ -7,8 +7,22 @@ import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 	
+	private final String ALERT_MSG = "Mobile Stage";
 	private Button submit;
     private SearchView searchRadius;
+	
+	private void alert(String msg) {
+	AlertDialog alertDialog = new AlertDialog.Builder(InputActivity.this).create();
+	alertDialog.setTitle(ALERT_MSG);
+	alertDialog.setMessage(msg);
+	alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+			new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			});
+	alertDialog.show();
+    }
 
 
     @Override
@@ -19,7 +33,10 @@ public class MainActivity extends AppCompatActivity {
 		submit = (Button) findViewById(R.id.search);
 		searchRadius = (SearchView) findViewById(R.id.radius);
 		
-		submit.setOnClickListener(new SubmitListener(this, String.valueOf(searchRadius.getQuery())));
+		submit.setOnClickListener(new SubmitListener(this, "ABC"));
+		
+		alert("Test");
+		alert(String.valueOf(searchRadius.getQuery()));
     }
 
 
